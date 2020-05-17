1fca2885216cf4d1cbf0e479a1791644c5bbc352
boda49139261 <60327895+boda49139261@users.noreply.github.com>
2020-05-17 22:19:18 +0300

    added lab1

---
+++ src/oop/laba1.java
@@ -0,0 +1,85 @@
+package oop;
+
+public class laba1 {
+    public static void main(String[] args) {
+        //2.5.1
+        int a = 5;
+        int b = -7;
+        int c = 12345678;
+        double d = 027;
+        int e = -0326;
+        float f = 0777f;
+        int g = 0xff0a;
+        int h = 0xFC2D;
+        long i = 25L;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+
+        //2.5.3
+
+        System.out.println('\123');
+        System.out.println('\346');
+        System.out.println('\u0053');
+        System.out.println('\u0416');
+
+        //2.5.4
+
+        System.out.println("Цей рядок\nз переносом");
+        System.out.println("\"Карпати\" — Чемпіон!");
+        System.out.println(" Зєднання " + "рядків");
+
+        //2.7.2
+
+        Boolean k = true;
+        Boolean m = false;
+        System.out.println(2 > 3);
+        System.out.println(4 == 4);
+        System.out.println(3 <= 2);
+
+        //2.7.3
+
+        System.out.println((5 != 0) && (8 / 5 > 0.001));
+        System.out.println((5 == 0) || (8 / 5 > 0.001));
+
+        //2.7.4
+
+        byte b1 = 50, b2 = -99, bЗ;
+        short det = 0, ind = 1;
+        int nm = -100, j = 100, kl = 9999;
+        long big = 50, veryBig = 2147483648L;
+        char c1 = 'A', c2 = '?', newLine = '\n';
+
+        //2.7.5.1
+
+        int M = 0;
+        System.out.println(M++);
+        System.out.println(M--);
+        System.out.println(M + 1999);
+        System.out.println(M * 67);
+        System.out.println(M - 4578);
+
+        //2.7.5.2
+        //2.7.5.3
+        //2.7.5.4
+        //2.7.5.5
+        //2.7.6
+        double ink = 0.001;
+        double kip = -34.789;
+        double fon = -16.2305, z2;
+        //2.7.7
+        byte hg = 1;
+        System.out.println(h += 10);
+        //2.7.8
+    }
+}
+
+
+
+
